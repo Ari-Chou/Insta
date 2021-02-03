b9e789aa4643118fd16760e125407b2447cc3644
AriChou <arichou.dev@gmail.com>
2021-02-03 16:03:19 +0800

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by AriChou on 2/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
